4c50b113c38f8ad1f4697dbac62150555af00c0b
Aldarien <aldarien85@gmail.com>
2017-10-10 18:47:44 -0300

    Added date formats

--- app/Helper/Format.php
+++ app/Helper/Format.php
@@ -3,17 +3,27 @@ namespace App\Helper;
 
 class Format
 {
-	public static function number($number, $decimals)
+	public static function number(float $number, $decimals)
 	{
 		return number_format($number, $decimals, ',', '.');
 	}
-	public static function pesos($number)
+	public static function pesos(float $number)
 	{
 		return self::number($number, 0);
 	}
-	public static function ufs($number)
+	public static function ufs(float $number)
 	{
 		return self::number($number, 2);
+	}
+	public static function date(string $date)
+	{
+		$d = new \Carbon\Carbon::parse($date, config('app.timezone'));
+		return $d->format("d \d\e F Y");
+	}
+	public static function shortDate(string $date)
+	{
+		$d = new \Carbon\Carbon::parse($date, config('app.timezone'));
+		return $d->format('d-m-Y');
 	}
 }
 ?>
