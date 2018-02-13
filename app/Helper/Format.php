7a719be989d55003c7cbfc5153ea7ab0a31a1d8c
Aldarien <aldarien85@gmail.com>
2018-02-13 12:13:00 -0300

    Added localDate, m2 and percent formats, and print option

--- app/Helper/Format.php
+++ app/Helper/Format.php
@@ -7,23 +7,37 @@ class Format
 	{
 		return number_format($number, $decimals, ',', '.');
 	}
-	public static function pesos(float $number)
+	public static function pesos(float $number, bool $print = false)
 	{
-		return self::number($number, 0);
+		return (($print) ? '$ ' : '') . self::number($number, 0);
 	}
-	public static function ufs(float $number)
+	public static function ufs(float $number, bool $print = false)
 	{
-		return self::number($number, 2);
+		return self::number($number, 2) . (($print) ? ' UF' : '');
 	}
 	public static function date(string $date)
 	{
 		$d = \Carbon\Carbon::parse($date, config('app.timezone'));
-		return $d->format("d \d\e F Y");
+		return $d->format("d \d\\e F Y");
 	}
 	public static function shortDate(string $date)
 	{
 		$d = \Carbon\Carbon::parse($date, config('app.timezone'));
 		return $d->format('d-m-Y');
+	}
+	public static function localDate(string $date)
+	{
+		$d = \Carbon\Carbon::parse($date, config('app.timezone'));
+		setlocale(LC_TIME, 'es');
+		return $d->formatLocalized('%d de %B de %Y');
+	}
+	public static function m2(float $number, bool $print = false)
+	{
+		return self::number($number, 2) . (($print) ? ' m&#0178;' : '');
+	}
+	public static function percent(float $number, bool $print = false)
+	{
+		return self::number($number, 2) . (($print) ? '%' : '');
 	}
 }
 ?>
